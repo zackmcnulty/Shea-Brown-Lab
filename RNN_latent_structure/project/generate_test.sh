872b07a2d917e56f8b101b74a4ad339f0725d702
Zachary McNulty <zmcnulty@uw.edu>
2019-07-16 09:31:56 -0700

    rewrote generate script for testing data to choose arbitrary points instead of angles

--- RNN_latent_structure/project/generate_test.sh
+++ RNN_latent_structure/project/generate_test.sh
@@ -6,27 +6,22 @@
 # ./generate_uniform.sh
 
 
-num_movies=180
+num_movies=10
 # number of movies to make with angles uniformly between [start, stop]
 folder="random" # foldername in ./test_movies to save movies
 
 
-
-diff=$(expr $stop_angle - $start_angle)
-step_size=$(expr $diff / $num_movies)
 
 touch "./test_movies/$folder/labels.csv"
 
 # generate training movies
 for i in $( seq $num_movies)
 do
-	angle=$(($start_angle + $(($i * $step_size))))
-       filename=`printf uniform%03d $angle`
-       python3 make_movie.py --name $filename --theta $angle --folder "./test_movies/$folder"
+       filename=`printf random%03d $i`
+       points=`python3 make_movie.py --name $filename --folder "./test_movies/$folder" --random_points`
 
        # add the given label to the labels file
-       printf $angle >> "./test_movies/$folder/labels.csv"
-       printf ',' >> "./test_movies/$folder/labels.csv"
+       printf "$i,$points\n" >> "./test_movies/$folder/labels.csv"
 done
 
-printf '%s\n' '$' 's/.$//' wq | ex './test_movies/uniform/labels.csv' 
+#printf '%s\n' '$' 's/.$//' wq | ex './test_movies/uniform/labels.csv' 
